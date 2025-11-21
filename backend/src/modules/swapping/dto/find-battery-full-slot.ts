aab40bcda6bc66cc681c788b667973a7e5b26011
vlnguyen910 <volamnguyen447@gmail.com>
2025-11-21 21:20:10 +0700

    fix: correct validation decorator for cabinet_id in FindBatteryFullSlotDto

--- backend/src/modules/swapping/dto/find-battery-full-slot.ts
+++ backend/src/modules/swapping/dto/find-battery-full-slot.ts
@@ -3,6 +3,6 @@ import { FindEmptySlotDto } from "./find-empty-slot.dto";
 
 export class FindBatteryFullSlotDto extends FindEmptySlotDto {
     @IsNotEmpty({ message: 'cabinet_id is required' })
-    @IsNotEmpty({ message: 'cabinet_id must be an integer' })
+    @IsNumber({ message: 'cabinet_id must be an integer' })
     cabinet_id: number;
 }
